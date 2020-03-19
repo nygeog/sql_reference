e78ab669ff515753f2fb45e1c3fd4d3a7033b5f1
Danny S <daniel.martin.sheehan@gmail.com>
2020-03-19 11:05:08 -0400

    Create insert_into.sql

---
+++ insert_into.sql
@@ -0,0 +1,7 @@
+-- Insert into columns in order:
+INSERT INTO table_name
+VALUES (value1, value2);
+
+-- Insert into columns by name:
+INSERT INTO table_name (column1, column2)
+VALUES (value1, value2);
